11846701c67c65fbd303c73c44ada328f9c9862a
Sabbir_Hossen <sh3235662@gmail.com>
2025-07-18 21:03:42 +0600

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold text-center my-4">
+                Hello world!
+            </h1>
+        </>
+    )
+}
+
+export default Navbar
